37b025fe35b3469d4464704014d43ca5d4488e71
lverghese <loganverghese@gmail.com>
2022-04-04 16:00:42 -0400

    maa classname:

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -11,7 +11,7 @@ import Pricing from './components/Pricing';
 //this will generate all class names in production for marketing app with a prefix
 // of 'ma' instead of 'jss1,2,3,etc'
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma',
+  productionPrefix: 'maa',
 });
 
 export default () => {
