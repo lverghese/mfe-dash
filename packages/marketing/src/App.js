ac8f25f49fc17ea3191722b7378f6ade60a36531
lverghese <loganverghese@gmail.com>
2022-04-04 14:24:25 -0400

    randomize prod classnames

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+//this will generate all class names in production for marketing app with a prefix
+// of 'ma' instead of 'jss1,2,3,etc'
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
