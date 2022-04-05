504e63812c43133161c38513270c0af15afed510
lverghese <loganverghese@gmail.com>
2022-04-05 18:44:38 -0400

    memoryhistory and browserhistory in prod and dev

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,29 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname}) => {
+        //this where we figure out what url the marketing app navigated to
+        //then use that to update the browser history in container
+        const { pathname } = history.location;
 
+        //checking if path name is different than last pathname
+        if( pathname !== nextPathname) {
+          history.push(nextPathname);
+        } 
+      },
+    });
+
+    history.listen(onParentNavigate);
+
+  }, []);
+    //the empty array as the second argument means only try to run this function
+    //when our marketingApp component is first rendered to the screen
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+// onNavigate is the key inside of the callback function argument
+const mount = (el, { onNavigate, defaultHistory }) => {
+
+  //if we provided a default history, use it, if not, use memory
+  // || means 'or'
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  //the history object has an event listener tied to it 
+  //called 'listen
+  //whenever some navigation occurs, this history object
+  // is going to call any function that we provided to this listen
+
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    //inside of here, we are going to place some number of functions
+    //that our container project can call to somehow change or update
+    //our marketing app
+
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation,
@@ -13,7 +43,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
