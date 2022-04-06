faf5e563aa4b3bcf6b790adcdc0c8820fcdc62d2
lverghese <loganverghese@gmail.com>
2022-04-06 14:02:52 -0400

    token error test

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -8,24 +8,24 @@ export default ({ onSignIn }) => {
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
-        initialPath: history.location.pathname,
+      initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
         //this where we figure out what url the marketing app navigated to
         //then use that to update the browser history in container
         const { pathname } = history.location;
 
         //checking if path name is different than last pathname
-        if( pathname !== nextPathname) {
+        if (pathname !== nextPathname) {
           history.push(nextPathname);
-        } 
+        }
       },
       onSignIn,
     });
 
     history.listen(onParentNavigate);
-
   }, []);
     //the empty array as the second argument means only try to run this function
     //when our marketingApp component is first rendered to the screen
   return <div ref={ref} />;
 };
+  
