8e7c0c4e4316292d5fce813e6cd9b7b616b458ed
lverghese <loganverghese@gmail.com>
2022-04-17 13:00:56 -0400

    dash app typo fix for integration

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
       name: 'dashboard',
       filename: 'remoteEntry.js',
       exposes: {
-        './DashApp': './src/bootstrap',
+        './DashboardApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
